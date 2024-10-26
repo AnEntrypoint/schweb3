6e32cbbb8d9b9673d5639db230ce0e1652d48cd7
web3genie <wxzvrd@protonmail.com>
2024-10-26 17:01:49 +0100

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -3,7 +3,8 @@ import adapter from '@sveltejs/adapter-auto';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-    kit: {
+    outDir: '/app/build',
+	kit: {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
