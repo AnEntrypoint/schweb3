72900d907590f069c754c334462ecff2d8cd6b9b
jb0gie <wxzvrd@protonmail.com>
2024-10-26 17:21:33 +0100

    okie dokie

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -11,15 +11,15 @@
 <div class="h-screen w-auto">
 	<!-- <Studio enabled={dev} /> -->
 
-	<Canvas>
-		<!-- <Sky 
+	<!-- <Canvas>
+		<Sky 
 			elevation={1} 
 			rayleigh={0.01}
 			turbidity={0}
 			exposure={0.37}
-		/> -->
+		/>
 		<Scene />
-	</Canvas>
+	</Canvas> -->
 	<div class="absolute top-10 left-0">
 		<div class="relative top-6 left-6">
 			<div class="text-purple text-sm font-bold uppercase">point emerging</div>
